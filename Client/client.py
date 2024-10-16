a5672d9848f273d2552adbb9decd29db418b3508
Gus1995 <gustavo.schuster@live.com>
2024-10-16 20:45:53 -0300

    ll

--- Client/client.py
+++ Client/client.py
@@ -9,10 +9,10 @@ def home():
     player1_mana = {'{B}': 0, '{W}': 2,'{G}': 0,'{R}': 0,'{U}': 0}
 
     hand_cards = [
-        {'name': 'Card 3', 'image_url': url_for('static', filename='7ed-84-mahamoti-djinn.jpg')},
+        {'name': 'Card 1', 'image_url': url_for('static', filename='7ed-84-mahamoti-djinn.jpg')},
         {'name': 'Card 2', 'image_url': url_for('static', filename='2ed-39-savannah-lions.jpg')},
         {'name': 'Card 3', 'image_url': url_for('static', filename='itp-54-plains.jpg')},
-        {'name': 'Card 3', 'image_url': url_for('static', filename='itp-54-plains.jpg')},
+        {'name': 'Card 4', 'image_url': url_for('static', filename='itp-54-plains.jpg')},
     ]
 
     return render_template('home.html', cards=hand_cards, player1_mana=player1_mana)
